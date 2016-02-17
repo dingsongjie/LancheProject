cf77e1842aeb5238155ad6f1bb92228c4a269d6f
dsj <377973147@qq.com>
2016-02-17 14:13:40 +0800

    move asyncHelper

--- Lanche.UnitOfWork/UnitOfWorkInterceptor.cs
+++ Lanche.UnitOfWork/UnitOfWorkInterceptor.cs
@@ -1,9 +1,11 @@
 ﻿using Castle.DynamicProxy;
 using Lanche.Core.Thread;
+using Lanche.UnitOfWork.Thread;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace Lanche.UnitOfWork
 {
