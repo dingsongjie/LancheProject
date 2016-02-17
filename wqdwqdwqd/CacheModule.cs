8e001b5ee8ae1c2d2bad3e0704273c5df35daaf3
dsj <377973147@qq.com>
2016-02-17 13:42:15 +0800

    1

---
+++ wqdwqdwqd/CacheModule.cs
@@ -0,0 +1,20 @@
+﻿using Lanche.Core;
+using Lanche.Core.Module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lanche.Cache
+{
+    [DependsOn(typeof(CoreModule))]
+    public class CacheModule:Module
+    {
+        public override void PreInitialize()
+        {
+            IocManager.IocContainer.Install(new CacheInstaller());
+            base.PreInitialize();
+        }
+    }
+}
