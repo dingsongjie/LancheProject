d76ddd665e509da15e6395f6f247305041723054
dingsongjie <377973147@qq.com>
2016-02-17 11:54:27 +0800

    1

---
+++ Lanche.Caches/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lanche.Caches
+{
+    public class Class1
+    {
+    }
+}
