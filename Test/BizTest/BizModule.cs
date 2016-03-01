97a9972ea37a357c1b7271edf7a65979723ef042
dsj <377973147@qq.com>
2016-03-01 14:20:11 +0800

    添加测试

--- Test/BizTest/BizModule.cs
+++ Test/BizTest/BizModule.cs
@@ -1,8 +1,13 @@
 ﻿using Lanche.Core.Application;
 using Lanche.Core.Module;
+using Lanche.DynamicWebApi;
 using Lanche.DynamicWebApi.Application;
 using Lanche.DynamicWebApi.Controller.Dynamic.Builders;
+using Lanche.Entityframework;
+using Lanche.MessageQueue;
+using Lanche.MongoDB;
 using Lanche.RabbitMq;
+using Lanche.Redis.RedisCache;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +17,11 @@ using System.Threading.Tasks;
 
 namespace BizTest
 {
+    [DependsOn(typeof(AbpWebApiModule)
+        , typeof(EntityframeworkMudule)
+        , typeof(MessageQueueModule)
+        , typeof(RedisCacheModule)
+        , typeof(MongoDbModule))]
     public class BizModule :Lanche.Core.Module. Module
     {
         public override void Initialize()
