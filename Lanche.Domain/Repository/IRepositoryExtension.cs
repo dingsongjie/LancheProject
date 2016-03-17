58bbb8eb69e28145f15152a68510f7db8e677e72
dsj <377973147@qq.com>
2016-03-17 15:56:42 +0800

    为 IRepository添加 insertOrUpdate扩展方法

--- Lanche.Domain/Repository/IRepositoryExtension.cs
+++ Lanche.Domain/Repository/IRepositoryExtension.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Lanche.Domain.Repository.Entity;
+using Lanche.Domain.Repository.Entity.Auditing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +17,7 @@ namespace Lanche.Domain.Repository
         /// <typeparam name="TEntity"></typeparam>
         /// <param name="repository"></param>
         /// <param name="entity"></param>
-        public async static Task<TEntity> InsertOrUpdateAsync<TEntity>(this IRepository<TEntity> repository, TEntity entity) where TEntity : Entity,new()
+       public async static Task<TEntity> InsertOrUpdateAsync<TEntity>(this IRepository<TEntity> repository, TEntity entity) where TEntity : ICreationAudited<string>, new()
         {
 
             var entityType = typeof(TEntity);
@@ -33,14 +36,47 @@ namespace Lanche.Domain.Repository
             if (dbEntity == null)
             {
 
-                return await repository.Insert4PlAsync(entity);
+                return await repository.InsertAsync(entity);
             }
             else
             {
 
                 var result = OverRideProperties(dbEntity, entity);
-                return await repository.Update4PlAsync(result);
+                return await repository.UpdateAsync(result);
             }
         }
+       /// <summary>
+       /// 新增或更新 ,根据传入实体的Id去数据库查询 若有Id相同的数据则自动判断为更新，否则则为新加，和数据库交互为2次，效率不高，若要比较高的效率请用 Insert 或Update方法
+       /// </summary>
+       /// <typeparam name="TEntity"></typeparam>
+       /// <param name="repository"></param>
+       /// <param name="entity"></param>
+       public  static TEntity InsertOrUpdateAsync<TEntity>(this IRepository<TEntity> repository, TEntity entity) where TEntity : ICreationAudited<string>,EntityBase<string>, new()
+       {
+     
+           var dbEntity =  repository.FirstOrDefault(m=>m.Id==entity.Id);
+           //新增
+           if (dbEntity == null)
+           {
+
+               return  repository.Insert(entity);
+           }
+           else
+           {
+
+               var result = OverRideProperties(dbEntity, entity);
+               return  repository.Update(result);
+           }
+       }
+       private static TEntity OverRideProperties<TEntity>(TEntity entityDb, TEntity entityNew) where TEntity : ICreationAudited<string>, new()
+       {
+           var type = entityDb.GetType();
+           var properties = type.GetProperties();
+           foreach (var pro in properties)
+           {
+               pro.SetValue(entityDb, pro.GetValue(entityNew));
+           }
+           return entityDb;
+       }
     }
 }
